3d23f6ea74ec017627ed26c88481dc1ab5546983
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-05-14 01:02:23 +0600

    Loading added

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,9 +1,20 @@
-import Map from "../Root/Map";
+import { useContext } from "react";
+
 import MyMap from "../Root/MyMap";
 import Slider from "../Root/Slider";
 import FeatureRooms from "./FeatureRooms/FeatureRooms";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const Home = () => {
+    const {loading}=useContext(AuthContext)
+    if (loading) {
+        return  <div className="flex gap-3 items-center justify-center min-h-screen">
+              <span className="loading loading-ball loading-xs"></span>
+              <span className="loading loading-ball loading-sm"></span>
+              <span className="loading loading-ball loading-md"></span>
+              <span className="loading loading-ball loading-lg"></span>
+          </div>
+      }
     return (
         <>
             <Slider></Slider>
