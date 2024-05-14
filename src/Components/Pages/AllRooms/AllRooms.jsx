44e791ed08678675dd8abd2d304eb6cf61773498
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-05-14 07:24:45 +0600

    Sort Complete

--- src/Components/Pages/AllRooms/AllRooms.jsx
+++ src/Components/Pages/AllRooms/AllRooms.jsx
@@ -1,29 +1,64 @@
 import { useLoaderData } from "react-router-dom";
 import Room from "../FeatureRooms/Room";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../AuthProvider/AuthProvider";
 import { Helmet } from "react-helmet";
 
 
 const AllRooms = () => {
-    const {loading}=useContext(AuthContext)
-    const allRooms = useLoaderData()
+    const { loading, apiLink } = useContext(AuthContext);
+    const allRooms = useLoaderData();
+    const [sortRoom, setSortRoom] = useState([]);
+
+    const [sort, setSort] = useState(false)
+
+    const handelSortRoom = () => {
+        setSort(!sort)
+    }
+    useEffect(() => {
+        fetch(`${apiLink}/allroomsbysort`)
+            .then(res => res.json())
+            .then(data => setSortRoom(data))
+    }, [apiLink])
+
     if (loading) {
-        return  <div className="flex gap-3 items-center justify-center min-h-screen">
-              <span className="loading loading-ball loading-xs"></span>
-              <span className="loading loading-ball loading-sm"></span>
-              <span className="loading loading-ball loading-md"></span>
-              <span className="loading loading-ball loading-lg"></span>
-          </div>
-      }
+        return <div className="flex gap-3 items-center justify-center min-h-screen">
+            <span className="loading loading-ball loading-xs"></span>
+            <span className="loading loading-ball loading-sm"></span>
+            <span className="loading loading-ball loading-md"></span>
+            <span className="loading loading-ball loading-lg"></span>
+        </div>
+    }
     return (
-        <div>
+        <div className="bg-blue-100 rounded-2xl ">
             <Helmet><title>Travel with Tanvir | All Room</title></Helmet>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5 bg-blue-100 rounded-2xl">
+            <div className="flex justify-end">
+            <button onClick={handelSortRoom} className="btn btn-outline m-4">{sort? "Sort by Cost":"Default"}</button>
+            </div>
+            {
+                sort ?
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5 ">
+                        {
+                            allRooms.map(room => <Room key={room._id} room={room}></Room>)
+                        }
+                    </div>
+                    :
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5 ">
+                        {
+                            sortRoom.map(room => <Room key={room._id} room={room}></Room>)
+                        }
+                    </div>
+            }
+            {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5 ">
                 {
                     allRooms.map(room => <Room key={room._id} room={room}></Room>)
                 }
             </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5 ">
+                {
+                    allRooms.map(room => <Room key={room._id} room={room}></Room>)
+                }
+            </div> */}
         </div>
     );
 };
